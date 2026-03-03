f291faa9eebe122ec431c024232f737d1bd1ad82
priverop <priverop@users.noreply.github.com>
2026-03-03 13:12:55 +0000

    fix(routes): remove home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get "get_competition_info", to: "competition#get_competition_info"
-  get "home", to: "pages#home"
-  root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
